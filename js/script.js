5a27cc5c252a30da7fc2fefadcc5b290144a037e
Oliver Gosling <oliver.gosling@bigfan.agency>
2019-07-03 16:13:03 +0100

    random quote generator

--- js/script.js
+++ js/script.js
@@ -3,54 +3,93 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+// Array of quote objects
+var quotes = [
+  {
+    quote: "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+    source: "Marilyn Monroe",
+    citation: "",
+    year: "",
+    category: "Movies",
+    backgroundColor: ""
+  },
+  {
+    quote: "Be the change that you wish to see in the world.",
+    source: "Mahatma Gandhi",
+    citation: "",
+    year: "",
+    category: "Politics",
+    backgroundColor: "#DB93B0"
+  },
+  {
+    quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    source: "Ralph Waldo Emerson",
+    citation: "",
+    year: "",
+    backgroundColor: "#F7BFB4"
+  },
+  {
+    quote: "I am so clever that sometimes I don't understand a single word of what I am saying.",
+    source: "Oscar Wilde",
+    citation: "The Happy Prince and Other Stories",
+    year: "1896",
+    category: "Literary",
+    backgroundColor: "#7DAF9C"
+  },
+  {
+    quote: "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+    source: "Maya Angelou",
+    citation: "",
+    year: "",
+    backgroundColor: "#23967F"
+  }
+];
+
+// Select a random quote object from quotes array
+function getRandomQuote(){
+  var randomNumber = Math.floor(Math.random() * quotes.length);
+  var randomQuote = quotes[randomNumber];
+  return randomQuote;
+}
 
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
+// Fetch a random quote, format it using HTML and then display it
+function printQuote(){
+  var randomQuote = getRandomQuote();
+  var htmlString = "";
 
+  // Using ES6 Template Strings
+  htmlString += `<p class="quote">${randomQuote.quote}</p>`;
+  htmlString += `<p class="source">${randomQuote.source}`;
 
+  if(randomQuote.citation){
+    htmlString += `<span class="citation">${randomQuote.citation}</span>`;
+  }
 
+  if(randomQuote.year){
+    htmlString += `<span class="year">${randomQuote.year}</span>`;
+  }
 
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
+  htmlString += '</p>';
 
+  if(randomQuote.category){
+    htmlString += `<p class="category">${randomQuote.category}</p>`;
+  }
 
+  document.getElementById('quote-box').innerHTML = htmlString;
 
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
+  // Change background color if specified
+  if(randomQuote.backgroundColor){
+    document.body.style.backgroundColor = randomQuote.backgroundColor;
+  }
+  else{
+    document.body.style.backgroundColor = "";
+  }
+}
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+// Change quote every 3 seconds
+setInterval(function(){
+  printQuote()
+}, 3000);
